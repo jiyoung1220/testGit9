849877f7f3954f0ca4d642f56a3d67b08d00b072
jy <jjyung1220@gmail.com>
2022-09-27 00:45:34 +0900

    [feature] create

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("test");
         //dev create : remote 커밋
+        //feature create // v.0.1.1
     }
 }
